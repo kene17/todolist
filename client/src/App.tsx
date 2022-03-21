316dab876145817050e4d55d310efc0f0666f318
charles <charles.obasi@nuenergy.ai>
2022-03-20 21:06:15 -0400

    navbar

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
-
+import Home from './pages/Home'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      let's go
+      <Home />
     </div>
   );
 }
---
+++ client/src/components/NavBar.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const NavBar = () => {
+  return (
+      <header className='header'>
+          <div className='header__logo'> TODOLIST </div>
+          <nav className='header__nav'>
+              <ul> <li>All Quotes</li> <li> Add Quote</li></ul>
+              </nav>
+          </header>
+  )
+}
+
+export default NavBar
